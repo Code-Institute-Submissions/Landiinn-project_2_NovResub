a6a5980f7c347c3f4cd641f76f1992239d64e2f7
Landiinn <andre.landin17@gmail.com>
2022-11-11 12:12:45 +0000

    creating functions to rungame

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,40 @@
-const playBtn = document.querySelectorAll(".playBtn");
-const resultText = document.querySelector("result");
-const rounds = document.querySelector("rounds");
-
-const userscore = document.querySelector("user_score");
-const compscore = document.querySelector("comp_score");
-
-
-let player;
-let computer;
-let result
-
-playBtn.forEach(button => button.addEventListener("click", () => {
-    player = button.textContent;
-   console.log(player);
-}))
-
-
+const game = () => {
+    let userScore = 0;
+    let computerScore = 0;
+    let rounds = 0;
+ 
+ 
+    
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const lizardBtn = document.querySelector('.lizard');
+        const spockBtn = document.querySelector('.spock');
+        const userChoice = [rockBtn,paperBtn,scissorBtn,lizardBtn,spockBtn];
+        const computerChoice = ['rock','paper','scissors','lizard','spock'];
+         
+        // StartGame Function
+        userChoice.forEach(choice => {
+            choice.addEventListener('click',function(){
+ 
+                const roundsLeft = document.querySelector('.rounds_left');
+                rounds++;
+                roundsLeft.innerText = `Rounds Left: ${10-moves}`;
+                 
+ 
+                const choiceNumbers = Math.floor(Math.random()*5) + 1;
+                const computerChoices = computerChoice[choiceNumbers];
+ 
+                // Check winner
+                winner(this.innerText,computerChoices)
+                 
+                // GameOver after 10 moves
+                if(rounds == 10){
+                    gameOver(playerOptions,roundsLeft);
+                }
+            })
+        })
+         
+    }
+}
